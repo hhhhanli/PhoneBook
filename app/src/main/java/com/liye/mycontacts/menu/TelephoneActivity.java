4cd8ad63d8d060e41bff1e95c5599f71e3d3f1f9
IMSEMZPZ <IMSEMZPZ@users.noreply.github.com>
2018-07-05 17:50:28 +0800

    test git

--- app/src/main/java/com/liye/mycontacts/menu/TelephoneActivity.java
+++ app/src/main/java/com/liye/mycontacts/menu/TelephoneActivity.java
@@ -40,6 +40,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+///////edit
 
 public class TelephoneActivity  extends FragmentActivity {
     /*************zjt:sidebar*************/
