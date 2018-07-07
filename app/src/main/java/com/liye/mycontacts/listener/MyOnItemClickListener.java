403292a0252cb3fed525a43248f4bb288c58cd17
yyh15336247 <1197435688@qq.com>
2018-07-07 17:15:08 +0800

    zwj

--- app/src/main/java/com/liye/mycontacts/listener/MyOnItemClickListener.java
+++ app/src/main/java/com/liye/mycontacts/listener/MyOnItemClickListener.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.AdapterView;
 
 import com.liye.mycontacts.menu.TelephoneActivity;
+import com.liye.mycontacts.myContacts.FirstFragment;
 
 /**
  * Created by MK on 2016/4/8.
@@ -13,15 +14,15 @@ import com.liye.mycontacts.menu.TelephoneActivity;
 侧滑菜单子条目的监听事件
  */
 public class MyOnItemClickListener implements AdapterView.OnItemClickListener {
-    private TelephoneActivity activity;
-    public MyOnItemClickListener(TelephoneActivity activity) {
-        this.activity = activity;
+    private FirstFragment fragment;
+    public MyOnItemClickListener(FirstFragment fragment) {
+        this.fragment = fragment;
     }
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
        // String text = activity.contentItems[position];
        // Toast.makeText(TelephoneActivity.this, text, Toast.LENGTH_SHORT).show();
-        FragmentTransaction tran1 = activity.getSupportFragmentManager().beginTransaction();
+        //FragmentTransaction tran1 = activity.getSupportFragmentManager().beginTransaction();
 
     }
     }
--- app/src/main/java/com/liye/mycontacts/myContacts/FirstFragment.java
+++ app/src/main/java/com/liye/mycontacts/myContacts/FirstFragment.java
@@ -1,17 +1,19 @@
-package com.liye.mycontacts.menu;
+package com.liye.mycontacts.myContacts;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.text.TextUtils;
 import android.util.Log;
-import android.widget.Button;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -25,9 +27,7 @@ import com.liye.mycontacts.listener.MListOnItemClickListener;
 import com.liye.mycontacts.listener.MyAddTextChangedListener;
 import com.liye.mycontacts.listener.MyOnItemClickListener;
 import com.liye.mycontacts.listener.MyOnclickListener;
-import com.liye.mycontacts.myContacts.AddPeopleActivity;
-import com.liye.mycontacts.myContacts.SearchEditText;
-import com.liye.mycontacts.myContacts.SideBar;
+import com.liye.mycontacts.menu.TelephoneActivity;
 import com.liye.mycontacts.utils.CommonUtil;
 import com.liye.mycontacts.utils.ContactInfo;
 import com.liye.mycontacts.utils.ContactsUtil;
@@ -38,119 +38,103 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-///////edit
-
-public class TelephoneActivity  extends FragmentActivity {
+public class FirstFragment extends android.support.v4.app.Fragment {
+    Context mContext;
     /*************zjt:sidebar*************/
     private SideBar sideBar;
     private TextView dialog;
     /*************zjt:sidebar*************/
-     //侧滑布局对象，用于通过手指滑动将左侧的菜单布局进行显示或隐藏。
+    //侧滑布局对象，用于通过手指滑动将左侧的菜单布局进行显示或隐藏。
     private ImageView ImageView_menu;
-    ProgressDialog mProgressDialog;// 圆形进度条的对话框
-    public static final int LOAD_FINISH = 0X01;
-    private List<Map<String, Object>> listitem;
     ListView mList;
     //侧滑中展示其它功能的
-    ListView mListShow;
     List<ContactInfo> filterContacts;
     ImageView mHead;
     ImageView mExit;
-    Button mAddText;
-    Button mCallphone;
+    TextView mAddText;
+    TextView mCallphone;
+    MainActivity activity;
 
     //////////////////////begin
-    Button mScanQrCode;
+    TextView mScanQrCode;
     public static final int GET_CODE = 0;
     ////////////////////end
-    ContactInfo mContactInfo;
+
     List<ContactInfo> contacts;
+
     ContactsUtil mContactsUtil;
     public DrawerLayout mDrawerLayout;
     public ContactAdapter adapter;
+
     SearchEditText mSearchEditText;
-    public String[] contentItems = {"Content Item a1", "Content Item 2", "Content Item 3",
-            "Content Item 4"};
-    String[] title = {"日历", "午后小憩", "听音乐", "心情记事本"};
-    int imageId[] = {R.drawable.calendar2, R.drawable.image30, R.drawable.tocas, R.drawable.pink_design};
-    Handler handler = new Handler() {
-        public void handleMessage(android.os.Message msg) {
-            if (msg.what == LOAD_FINISH) {
-                // 按首字母排序
-                Collections.sort(contacts, new PinyinComparator());
-                adapter = new ContactAdapter(TelephoneActivity.this, contacts);
-                mList.setAdapter(adapter);
-                // 取消进度条
-                mProgressDialog.dismiss();
-            }
-        }
-
-        ;
-    };
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_telephone);
-
-        // 显示进度条
-        mProgressDialog = ProgressDialog.show(this, "请稍等", "数据正在加载......");
-        listitem = new ArrayList<Map<String, Object>>();
-        mContactInfo = new ContactInfo();
-        // 将上述资源转化为list集合
-        for (int i = 0; i < title.length; i++) {
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("image", imageId[i]);
-            map.put("title", title[i]);
-            listitem.add(map);
-        }
-        new Thread() {
-            public void run() {
-                mContactsUtil = new ContactsUtil(TelephoneActivity.this);
-                contacts = mContactsUtil.select();
-                handler.sendEmptyMessage(LOAD_FINISH);
-            }
-        }.start();
-        //initView();
+    public FirstFragment() {
+        // Required empty public constructor
     }
 
-    public void initView() {
-        /*
-        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawerlayout);
-        mHead = (ImageView) findViewById(R.id.imagebtn);
-        getCircleHead();
-        mExit = (ImageView) findViewById(R.id.imagebtn1);
-        mExit.setOnClickListener(new MyOnclickListener(this));
-        mListShow = (ListView) findViewById(R.id.contentList);
-        ListViewAdapter adapter1 = new ListViewAdapter(this, listitem);
-        mListShow.setAdapter(adapter1);
-        mListShow.setOnItemClickListener(new MyOnItemClickListener(this));
+    public void SetContext(Context context) {
+        // Required empty public constructor
+        this.mContext = context;
+    }
 
-        ImageView_menu = (ImageView) findViewById(R.id.menu1);
+    public Context getCon() {
+        return this.mContext;
+    }
+
+    public void setAct(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    public MainActivity getAct() {
+        return this.activity;
+    }
+
+    public DrawerLayout getDra() {
+        return this.mDrawerLayout;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one, null);
+        mContactsUtil = new ContactsUtil(mContext);
+        contacts = mContactsUtil.select();
+        Collections.sort(contacts, new PinyinComparator());
+        adapter = new ContactAdapter(this.mContext, contacts);
+        mList = (ListView) view.findViewById(R.id.lst_show_contact);
+        mList.setAdapter(adapter);
+
+        mDrawerLayout=(DrawerLayout) view.findViewById(R.id.drawerlayout);
+        mHead = (ImageView) view.findViewById(R.id.imagebtn);
+
+        getCircleHead();
+        mExit = (ImageView) view.findViewById(R.id.imagebtn1);
+
+        mExit.setOnClickListener(new MyOnclickListener(this));
+
+        ImageView_menu = (ImageView) view.findViewById(R.id.menu1);
         ImageView_menu.setOnClickListener(new MyOnclickListener(this));
-        mList = (ListView) this.findViewById(R.id.lst_show_contact);
         mList.setOnItemClickListener(new MListOnItemClickListener(this));
-        mAddText = (Button) this.findViewById(R.id.txt_add_contact);
+        mAddText = (TextView) view.findViewById(R.id.txt_add_contact);
         mAddText.setOnClickListener(new MyOnclickListener(this));
-        mCallphone = (Button) this.findViewById(R.id.txt_call_phone);
+        mCallphone = (TextView) view.findViewById(R.id.txt_call_phone);
         mCallphone.setOnClickListener(new MyOnclickListener(this));
 
         ////////////begin/////
-        mScanQrCode = (Button) this.findViewById(R.id.txt_scan_QrCode);
+        mScanQrCode = (TextView) view.findViewById(R.id.txt_scan_QrCode);
         mScanQrCode.setOnClickListener(new MyOnclickListener(this));
         //////////////end///////////
 
-        mSearchEditText = (SearchEditText) this.findViewById(R.id.edt_search);
+        mSearchEditText = (SearchEditText) view.findViewById(R.id.edt_search);
         // 添加一个文本改变的监听事件
-        mSearchEditText.addTextChangedListener(new MyAddTextChangedListener(this));*/
+        mSearchEditText.addTextChangedListener(new MyAddTextChangedListener(this));
 
 
         /***********************************zjt:sidebar*****************/
-        sideBar = (SideBar) findViewById(R.id.mysidebar);
-        dialog = (TextView) findViewById(R.id.dialog);
+        sideBar = (SideBar) view.findViewById(R.id.mysidebar);
+        dialog = (TextView) view.findViewById(R.id.dialog);
         sideBar.setTextView(dialog);
 
         // 设置右侧触摸监听
@@ -166,10 +150,9 @@ public class TelephoneActivity  extends FragmentActivity {
 
             }
         });
+        return view;
     }
 
-
-    // 过滤联系人的信息
     public void filterContact(String input) {
         filterContacts = new ArrayList<ContactInfo>();
         // 如果输入是空的
@@ -202,7 +185,7 @@ public class TelephoneActivity  extends FragmentActivity {
 
     //////////////begin
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         //扫描结果回调
         if (requestCode == GET_CODE && resultCode == CaptureActivity.RESULT_CODE_QR_SCAN) {
@@ -217,11 +200,11 @@ public class TelephoneActivity  extends FragmentActivity {
                 String name = jsonObject.optString("name",invalid),phone = jsonObject.optString("phone",invalid),
                         email = jsonObject.optString("email",invalid),address = jsonObject.optString("address",invalid);
                 if(name.equals(invalid) || phone.equals(invalid)  ){
-                    Toast.makeText(TelephoneActivity.this,"二维码名片格式错误",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mContext,"二维码名片格式错误",Toast.LENGTH_SHORT).show();
 
                     return ;
                 }else{
-                    Intent intent = new Intent(TelephoneActivity.this,
+                    Intent intent = new Intent(mContext,
                             AddPeopleActivity.class);
                     Log.d("infooo1",scanResult);
                     intent.putExtra("contactInfo",scanResult);
@@ -229,10 +212,8 @@ public class TelephoneActivity  extends FragmentActivity {
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
-                Toast.makeText(TelephoneActivity.this,"二维码名片格式错误",Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext,"二维码名片格式错误",Toast.LENGTH_SHORT).show();
             }
         }
     }
-    /////////////////end
-
 }
